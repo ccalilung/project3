897339977290be9086bb77dca4adfaa5f57d4db9
ccalilung <brobeans289@gmail.com>
2018-10-20 11:58:56 -0400

    run build

---
+++ foodapp/build/precache-manifest.16aadaf6d5aadb590c72328447ec5afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51776a13dc7e5d2dcfb1",
+    "url": "/static/js/main.51776a13.chunk.js"
+  },
+  {
+    "revision": "01422e9f758dbcff3e0d",
+    "url": "/static/js/1.01422e9f.chunk.js"
+  },
+  {
+    "revision": "51776a13dc7e5d2dcfb1",
+    "url": "/static/css/main.255e6d44.chunk.css"
+  },
+  {
+    "revision": "671e7f2a0eba2d4460d0cf8b2848f4e3",
+    "url": "/index.html"
+  }
+];
